31d05095668f6681a874605b064dded60bea13f9
Andrii Shupta <Andrii_Shupta@epam.com>
2020-12-21 00:39:33 +0200

    improvement: search ignore case; Home height

--- apps/product-viewer-app/src/app/Home.tsx
+++ apps/product-viewer-app/src/app/Home.tsx
@@ -3,7 +3,7 @@ import { Box, Container, Typography } from '@material-ui/core';
 
 const Home = () => {
   return (
-    <Box display='flex' height='100vh' justifyContent='center' alignItems='center'>
+    <Box display='flex' height='calc(100vh - 65px)' justifyContent='center' alignItems='center'>
       <Container maxWidth='md'>
         <Typography component='h1' variant='h3'>Welcome!</Typography>
         <Typography variant='body1'>This is simple "Product View" Application that allow to show and list products
