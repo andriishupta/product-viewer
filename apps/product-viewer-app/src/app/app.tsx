83940604508ea78c93e50e9c9de8e34b30287b4d
Andrii Shupta <Andrii_Shupta@epam.com>
2020-12-20 18:32:04 +0200

    feat(app): Add Material-UI - add M-UI - update Products View

--- apps/product-viewer-app/src/app/app.tsx
+++ apps/product-viewer-app/src/app/app.tsx
@@ -1,27 +1,46 @@
 import React from 'react';
-import { BrowserRouter, Link, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Link as RouterLink, Redirect, Route, Switch } from 'react-router-dom';
+
+import { AppBar, CssBaseline, Link, Toolbar, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { Product, Products } from './product';
 import Home from './Home';
 
+const useStyles = makeStyles((theme) => ({
+  links: {
+    marginRight: theme.spacing(2)
+  }
+}));
+
 export const App = () => {
+  const classes = useStyles();
+
   return (
     <BrowserRouter>
+      <CssBaseline />
       <div className='app'>
-        <nav className='app-nav'>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-              <Link to='/products'>Products</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path='/' exact component={ Home } />
-          <Route path='/products' exact component={ Products } />
-          <Route path='/products/:id' exact component={ Product } />
-          <Redirect from="*" to='/' />
-        </Switch>
+        <header>
+          <AppBar position='relative' color='transparent'>
+            <Toolbar>
+              <Link to='/' component={ RouterLink } className={ classes.links }>
+                <Typography variant='h6' color='inherit'>Home</Typography>
+              </Link>
+              <Link to='/products' component={ RouterLink } className={ classes.links }>
+                <Typography variant='h6' color='inherit'>Products</Typography>
+              </Link>
+            </Toolbar>
+          </AppBar>
+        </header>
+
+        <main>
+          <Switch>
+            <Route path='/' exact component={ Home } />
+            <Route path='/products' exact component={ Products } />
+            <Route path='/products/:id' exact component={ Product } />
+            <Redirect from='*' to='/' />
+          </Switch>
+        </main>
       </div>
     </BrowserRouter>
   );
--- apps/product-viewer-app/src/app/product/Products.tsx
+++ apps/product-viewer-app/src/app/product/Products.tsx
@@ -1,7 +1,39 @@
 import React, { useEffect, useState } from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, Card, CardActions, CardContent, CardMedia, Container, Grid, Typography } from '@material-ui/core';
+
 import { Message } from '@product-viewer/api-interfaces';
 
+const useStyles = makeStyles((theme) => ({
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6)
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4)
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8)
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  cardMedia: {
+    paddingTop: '56.25%' // 16:9
+  },
+  cardContent: {
+    flexGrow: 1
+  }
+}));
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const Products = () => {
+  const classes = useStyles();
   const [m, setMessage] = useState<Message>({ message: '' });
 
   useEffect(() => {
@@ -11,22 +43,61 @@ const Products = () => {
   }, []);
 
   return (
-    <>
-      <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to product-viewer-app!</h1>
-        <img
-          width="450"
-          src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
-        />
+    <React.Fragment>
+      <div className={ classes.heroContent }>
+        <Container maxWidth='md'>
+          <Typography component='h1' variant='h2' color='textPrimary' gutterBottom>
+            Products
+          </Typography>
+          <div className={ classes.heroButtons }>
+            <Grid container spacing={ 2 }>
+              <Grid item>
+                <Button variant='contained' color='primary'>
+                  Search
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button variant='outlined' color='primary'>
+                  Vendors
+                </Button>
+              </Grid>
+            </Grid>
+          </div>
+        </Container>
       </div>
-      <div>{m.message}</div>
 
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-    </>
+      <Container className={ classes.cardGrid } maxWidth='md'>
+        <Grid container spacing={ 4 }>
+          { cards.map((card) => (
+            <Grid item key={ card } xs={ 12 } sm={ 6 } md={ 4 }>
+              <Card className={ classes.card }>
+                <CardMedia
+                  className={ classes.cardMedia }
+                  image='https://source.unsplash.com/random'
+                  title='Image title'
+                />
+                <CardContent className={ classes.cardContent }>
+                  <Typography gutterBottom variant='h5' component='h2'>
+                    Heading
+                  </Typography>
+                  <Typography>
+                    This is a media card. You can use this section to describe the content.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size='small' color='primary'>
+                    View
+                  </Button>
+                  <Button size='small' color='primary'>
+                    Edit
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          )) }
+        </Grid>
+      </Container>
+    </React.Fragment>
   );
 };
 
